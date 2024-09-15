16047fbe8bffd9be181554cccd1397054dd54662
AkshGupta2003 <gupta17aksh@gmail.com>
2024-09-15 21:05:57 +0530

    fixed error in gmail_service.py & added screenshots

--- gmail_service.py
+++ gmail_service.py
@@ -7,30 +7,30 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import os.path
 
-# If modifying these scopes, delete the file token.json.
+# If modifying these scopes, delete the file email_token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 def authenticate_gmail():
     """Authenticates the Gmail API."""
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('email_token.json'):
+        creds = Credentials.from_authorized_user_file('email_token.json', SCOPES)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('email_credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open('email_token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
 
-def create_email( recipient, subject, body):
+def create_email(sender, recipient, subject, body):
     """Creates an email message."""
     message = MIMEText(body)
     message['to'] = recipient
-    message['from'] =  "aksh.gupta.21cse@bmu.edu.in"
+    message['from'] =  sender
     message['subject'] = subject
     raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
     return {'raw': raw_message}
@@ -45,14 +45,14 @@ def send_email(service, user_id, email_message):
         print(f"An error occurred: {error}")
         return None
 
-def emailer_task( recipient, subject, body):
-    """The task function for sending an email."""
-    # Authenticate Gmail API
-    creds = authenticate_gmail()
-    service = build('gmail', 'v1', credentials=creds)
+# def emailer_task(sender, recipient, subject, body):
+#     """The task function for sending an email."""
+#     # Authenticate Gmail API
+#     creds = authenticate_gmail()
+#     service = build('gmail', 'v1', credentials=creds)
 
-    # Create email
-    email_message = create_email( recipient, subject, body)
+#     # Create email
+#     email_message = create_email(sender, recipient, subject, body)
 
-    # Send email
-    send_email(service, 'me', email_message)
+#     # Send email
+#     send_email(service, 'me', email_message)
